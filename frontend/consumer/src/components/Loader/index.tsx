973d497ca5df359c3e94857da2ea624e9b5199c7
NataliaAfricano <naaleafri@gmail.com>
2023-10-15 12:01:17 -0500

    Delete componentes unnecessary

--- frontend/consumer/src/components/Loader/index.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./style.module.scss";
-
-export const Loader = ({ size = 25 }: { size?: number }) => {
-  return (
-    <div style={{ width: size, height: size }} className={styles.spinner} />
-  );
-};
