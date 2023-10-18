7bdf8eb2c522fd86d19057e2760c8ec3e17da308
NataliaAfricano <naaleafri@gmail.com>
2023-10-17 20:12:57 -0500

    Components HomePage

--- frontend/consumer/src/app/home/components/principal.tsx
+++ frontend/consumer/src/app/home/components/principal.tsx
@@ -1,25 +1,79 @@
 import { useSession} from "next-auth/react";
+import  Card from "../components/cards";
 
 function principal (){
     const { data: session } = useSession();
     return(
         <div>
-            <div className="font-bold text-4xl m-2">Hello {session?.user?.name || "No user"} !</div>
-            <div className="font-bold text-2xl ml-16 m-3">Your buy number is</div>
+            <div className="font-bold text-4xl m-4">Hello {session?.user?.name || "No user"} !</div>
+            <div className="font-bold text-2xl ml-16 m-3">Your bay number is</div>
             <div className="text-right mr-20">
-                <div className="font-bold text-4xl mr-20">8:00am</div>
-                <div className="font-semi-bold text-xl mr-20">Starts at</div>
+                <div className="font-bold text-4xl mr-20"> -- : -- am</div>
+                <div className="font-semi-bold text-xl mr-12" style={{ display: "flex",justifyContent: "flex-end", alignItems: "center" }}>
+                    Starts at
+                    <div
+                        style={{
+                            backgroundColor: "blue",
+                            color: "white",
+                            width: "20px", // Ajusta el ancho del círculo
+                            height: "20px", // Ajusta la altura del círculo
+                            borderRadius: "50%",
+                            marginLeft: "10px", // Ajusta la distancia aquí
+                        }}
+                        />
+                        <div
+                            style={{
+                            position: "fixed",
+                            top: "247px",
+                            right: "145px",
+                            transform: "translateX(-50%)",
+                            width: "2px",
+                            height: "5.4cm",
+                            backgroundColor: "grey",
+                            border: "1px dashed white",
+                            }}
+                        />
+                </div>
+                
             </div>
             <div className="mx-auto text-center">
-                <div className="font-bold text-9xl">12A</div>
-                <div className="font-semi-bold text-xl">Building C, Floor 20</div>
+                <div className="font-bold text-9xl">---</div>
+                <div className="font-semi-bold text-xl">Building --, Floor --</div>
             </div>
             <div className="text-right mr-20">
-                <div className="font-bold text-4xl mr-20">5:00pm</div>
-                <div className="font-semi-bold text-xl mr-20">Ends at</div>
+                <div className="font-bold text-4xl mr-20">-- : -- pm</div>
+                <div className="font-semi-bold text-xl mr-12"style={{ display: "flex",justifyContent: "flex-end", alignItems: "center" }}>
+                    Ends at
+                    <div
+                        style={{
+                            backgroundColor: "blue",
+                            color: "white",
+                            width: "20px", // Ajusta el ancho del círculo
+                            height: "20px", // Ajusta la altura del círculo
+                            borderRadius: "50%",
+                            marginLeft: "10px", // Ajusta la distancia aquí
+                        }}
+                        />
+                </div>
             </div>
-            
-            
+
+            <div className="flex justify-center mt-5 ">
+                <Card
+                    title="Calendar"
+                    description="Check date on the calendar."
+                    href="/home"
+                />
+                <Card
+                    title="Reserve"
+                    description="Make your reservation."
+                    href="/home"
+                />
+                <Card
+                    title="Search"
+                    description="Search whatever you want."
+                    href="/home"
+                />
+            </div>
         </div>    
     );
 
--- frontend/consumer/src/app/home/page.tsx
+++ frontend/consumer/src/app/home/page.tsx
@@ -1,23 +1,15 @@
 'use client'
 
-import { useSession, signOut} from "next-auth/react";
-import { redirect } from "next/navigation";
 import Navbar from "../../components/NavBar/NavBar";
 import Principal from "./components/principal";
 
 function HomePage(){
-  const { data: session } = useSession();
-  const close = () => {
-    signOut({redirect:true, callbackUrl: "/"})
-  }
-
+ 
   return (
     <>
       <div><Navbar /></div>
       <div className="bg-white m-2">
         <div><Principal /></div>
-        <button onClick={close} className="m-2">Close session</button>
-
       </div>
     </>
   )
