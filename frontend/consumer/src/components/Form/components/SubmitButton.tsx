44852babf22fea410788bd0027d155a097c087cf
patata1311 <32421804+patata1311@users.noreply.github.com>
2023-10-18 17:13:55 -0500

    fix loader

--- frontend/consumer/src/components/Form/components/SubmitButton.tsx
+++ frontend/consumer/src/components/Form/components/SubmitButton.tsx
@@ -1,15 +1,21 @@
-import { Loader } from "@/components/Loader";
+
 import styles from "./styles.module.scss";
+const Loader: React.FC = () => {
+  return <div>Loading...</div>;
+}
 
 interface SubmitButtonProps {
-  buttonText: string;
-  isLoading?: boolean;
+buttonText: string;
+isLoading?: boolean;
 }
 
+
 export function SubmitButton({ buttonText, isLoading }: SubmitButtonProps) {
-  return (
-    <button className={styles.submitButton} type="submit" disabled={isLoading}>
-      {isLoading ? <Loader /> : buttonText}
-    </button>
-  );
+return (
+  <button className={styles.submitButton} type="submit" disabled={isLoading}>
+    {isLoading ? "Cargando..." : buttonText}
+  </button>
+);
 }
+
+
